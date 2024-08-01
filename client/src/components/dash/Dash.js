60bb44df5ee303bd7a16b70038c405133d37fa79
f101stars <f0548580123@gmail.com>
2024-08-01 10:36:46 +0300

    add basic dash

---
+++ client/src/components/dash/Dash.js
@@ -0,0 +1,4 @@
+const Dash = ()=>{
+    return <h1>dash</h1>
+}
+export default Dash
---
+++ client/src/components/layout/site/Site.js
@@ -0,0 +1,8 @@
+import {NavLink} from "react-router-dom"
+
+const Site = ()=>{
+    return <>
+    <NavLink to="/login">login</NavLink>
+    </>
+}
+export default Site
